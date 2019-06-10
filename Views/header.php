e0c96ef6fb430f65516e9592e0456afa8a79bbf2
Ivan Karlen <Ikarlen@alumno.unlam.edu.ar>
2019-06-10 15:07:36 -0300

    Busco datos por metodo Get

--- Views/header.php
+++ Views/header.php
@@ -86,18 +86,11 @@
                 </div>
                 <!-- /Logo -->
 
-                <!-- Search -->
+                 <!-- BUSCADOR -->
                 <div class="header-search">
-                    <form>
-                        <input class="input " type="text" placeholder="Enter your keyword"/>
-						
-					
-                       <!-- <select class="input search-categories">
-                            <option value="0">All Categories</option>
-                            <option value="1">Category 01</option>
-                            <option value="1">Category 02</option>
-                        </select>-->
-                        <button class="search-btn"><i class="fa fa-search"></i></button>
+                    <form method="get" action="ProductoBuscado.php">
+                        <input class="input" style="margin:0 auto; display:block;width:400px;" name="buscar" type="search" placeholder="Buscar productos, marcas y más" required />
+                        <button class="search-btn" name="enviar" type="submit"><i class="fa fa-search"></i></button>
                     </form>
                 </div>
 				
