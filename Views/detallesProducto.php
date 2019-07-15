95ba3dbf85f5c440134727145a0bb5b8b5bdc523
Duffy - <31010096+candekn@users.noreply.github.com>
2019-07-14 22:09:06 -0300

    Espero funcione amen

--- Views/detallesProducto.php
+++ Views/detallesProducto.php
@@ -256,47 +256,46 @@ while($f=mysqli_fetch_array($resultado)){
 
 					<form method="post" action="ProcesaAddCarrito.php">
 					<div class="col-md-6">
-						<div class="product-body">
-							<div class="product-label">
-								<span>New</span>
-								<span class="sale">-20%</span>
-							</div>
-							<h2 class="product-name"><?php echo $f["nombre"];?></h2>
-							<h3 class="product-price"><?php echo "$".number_format($f['precio'],0,'.','.');?> <del class="product-old-price" value="precio"><?php if($f["precio"]>1000){echo "$".$mostrar= number_format($f['precio']+ 1650,0,'.','.') ;}elseif($f["precio"]>10000){echo "$".$mostrar= number_format($f["precio"] + 5200,0,'.','.');}?></del></h3>
-							<div>
-								<div class="product-rating">
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star-o empty"></i>
-								</div>
-								<a href="#">3 Review(s) / Add Review</a>
-							</div>
-							<p><strong>Cantidad:</strong> <?php if($f["stock"]>0){echo "Dispone de Stock";}else{echo "Se ha vendido todo";}?></p>
-							<p><strong>Marca:</strong> <?php echo $f["marca"];?></p>
-							<p><?php echo $f["descripcion"];?></p>
-							<div class="product-options">
-								<ul class="size-option">
-									<li><span class="text-uppercase">Size:</span></li>
-									<li class="active"><a href="#">S</a></li>
-									<li><a href="#">XL</a></li>
-									<li><a href="#">SL</a></li>
-								</ul>
-								<ul class="color-option">
-									<li><span class="text-uppercase">Color:</span></li>
-									<li class="active"><a href="#" style="background-color:#475984;"></a></li>
-									<li><a href="#" style="background-color:#8A2454;"></a></li>
-									<li><a href="#" style="background-color:#BF6989;"></a></li>
-									<li><a href="#" style="background-color:#9A54D8;"></a></li>
-								</ul>
-							</div>
-
-							<div class="product-btns">
-								<div class="qty-input">
-									<span class="text-uppercase">Cantidad: </span>
-									<input id="numero" name="cantidad" class="input" type="number" min="1" pattern="^[0-9]+" required>
-								</div><br><br><br><br><br>
+						                            <div class="product-body">
+                                <div class="product-label">
+                                    <span>New</span>
+                                    <span class="sale">-20%</span>
+                                </div>
+                                <h2 class="product-name"><?php echo $f["nombre"]; ?></h2>
+                                <h3 class="product-price"><?php echo "$" . number_format($f['precio'], 0, '.', '.'); ?> </h3>
+                                <div>
+                                    <div class="product-rating">
+                                        <?php
+                                        $idU=$conexion->consultarIdUser($f['id']);
+                                        $cant=$conexion->cambiarTipoUser($idU);
+                                        $tipoU=$conexion->consultarTipoUser($idU);
+                                        echo "<h4 class='text-primary'>$tipoU</h4>";
+                                        for($i=0;$i<$cant;$i++){
+                                            echo "<i class='fa fa-star'></i>";
+                                        };
+                                        for($z=0;$z<5-$cant;$z++){
+                                            echo "<i class='fa fa-star-o empty'></i>";
+                                        };
+                                         ?>
+                                    </div>
+                                </div>
+                                <p><strong>Cantidad:</strong> <?php if ($f["stock"] > 0) {
+                                        echo "$f[stock]";
+                                    } else {
+                                        echo "No hay stock";
+                                    } ?></p>
+                                <p><strong>Marca:</strong> <?php echo $f["marca"]; ?></p>
+                                <p><?php echo $f["descripcion"]; ?></p>
+							            <div class="product-btns">
+                                    <div class="qty-input">
+                                        <span class="text-uppercase">Cantidad: </span>
+                                        <input id="numero" name="cantidad" class="input"
+                                               type="number" min="1"
+                                               <?php echo "max='$f[stock]'";
+                                               ?>pattern="^[0-9]+"
+                                               required>
+                                    </div>
+                                    <br><br><br><br><br>
 								
 								<button type="submit" name="AddCarrito" class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Añadir al carrito</button>
 								<input type="hidden" name="Add" value="<?php echo $f['id'];?>">
@@ -308,11 +307,7 @@ while($f=mysqli_fetch_array($resultado)){
 								<?php
 								$msj= isset($_GET["error"]) ? $_GET["error"] : "";
 								?>
-								<div class="pull-right">
-									<button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-									<button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-									<button class="main-btn icon-btn"><i class="fa fa-share-alt"></i></button>
-								</div>
+								
 							</div>
 						</div>
 					</div>
@@ -338,69 +333,63 @@ while($f=mysqli_fetch_array($resultado)){
 							</ul>
 							<div class="tab-content">
 								<div id="tab1" class="tab-pane fade in active">
-									<p><?php echo $f["descripcion"];?></p>
-								</div>
-								<div id="tab2" class="tab-pane fade in active">
-									<p><?php echo "Envio: ".$f["envio"];?></p>
-								</div>
-								<div id="tab3" class="tab-pane fade in">
+                      						 <p><?php echo "Descripción: " . $f["descripcion"]; ?></p>
+                        					<p><?php echo "Envio: " . $f["envio"]; ?></p>
+                   					 </div>
+								   <div id="tab2" class="tab-pane fade in">
+                        <?php
+                        if($f["latitud"]=="undefined"||$f["latitud"]==null){
+                            echo "<p>El vendedor no ha definido la ubicación del producto.</p>";
+                        }else{
+                           echo  "<div id='map' style='width: 100%; height:200px'></div>
+                                <input style='display: none' name='lat' type='number' id='lat' value='$f[latitud]'>
+                            <input style='display: none' name='lng' type='number' id='lng' value='$f[longitud]'>";
+                        }
+                        ?>
+                    </div>
+							                   <div id="tab3" class="tab-pane fade in">
+ <?php
 
+ } //fin del while de producto
+$valoraciones=$conexion->traerValoraciones($idU);
+?>
 									<div class="row">
 										<div class="col-md-6">
 											<div class="product-reviews">
-												<div class="single-review">
-													<div class="review-heading">
-														<div><a href="#"><i class="fa fa-user-o"></i> John</a></div>
-														<div><a href="#"><i class="fa fa-clock-o"></i> 27 DEC 2017 / 8:0 PM</a></div>
+                                        <?php while($v =
+                                            $valoraciones->fetch_assoc())
+                                        { ?>
+
+
+                                                <div class="single-review">
+                                                    <div class="review-heading">
+                                                        <?php
+                                                        $nu = "";
+                                                        $nu = $conexion->traerUsuarioQueValora
+                                                        ($v['idUsuario']);
+
+														echo "<div><h6><i class='fa
+														fa-user-o'></i> $nu </h6></div>";
+														?>
 														<div class="review-rating pull-right">
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star-o empty"></i>
+                                                            <?php
+                                                            for($x=0;$x<$v['puntaje'];$x++) {
+                                                                echo "<i class='fa fa-star'></i>";
+                                                            };
+                                                            for($z=0;$z<5-$v['puntaje'];$z++){
+                                                                echo "<i class='fa fa-star-o empty'></i>";
+                                                            };
+															?>
 														</div>
 													</div>
 													<div class="review-body">
-														
+                                                        <?php echo
+                                                        "<p>$v[comentario]</p>"; ?>
 													</div>
-												</div>
-
-												<div class="single-review">
-													<div class="review-heading">
-														<div><a href="#"><i class="fa fa-user-o"></i> John</a></div>
-														<div><a href="#"><i class="fa fa-clock-o"></i> 27 DEC 2017 / 8:0 PM</a></div>
-														<div class="review-rating pull-right">
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star-o empty"></i>
-														</div>
-													</div>
-													<div class="review-body">
-														<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute
-															irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-													</div>
-												</div>
-
-												<div class="single-review">
-													<div class="review-heading">
-														<div><a href="#"><i class="fa fa-user-o"></i> John</a></div>
-														<div><a href="#"><i class="fa fa-clock-o"></i> 27 DEC 2017 / 8:0 PM</a></div>
-														<div class="review-rating pull-right">
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star-o empty"></i>
-														</div>
-													</div>
-													<div class="review-body">
-														<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute
-															irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-													</div>
-												</div>
-
+												</div> <!--/single review-->
+                                        <?php } //fin for
+                                        $valoraciones->close();
+                                        ?>
 												<ul class="reviews-pages">
 													<li class="active">1</li>
 													<li><a href="#">2</a></li>
@@ -410,27 +399,25 @@ while($f=mysqli_fetch_array($resultado)){
 											</div>
 										</div>
 										<div class="col-md-6">
-											<h4 class="text-uppercase">Write Your Review</h4>
-											<p>Your email address will not be published.</p>
-											<form class="review-form">
+											<h4 class="text-uppercase">Tu reseña</h4>
+											<form class="review-form" method="post"
+                                                  action="procesarValoracion.php">
 												<div class="form-group">
-													<input class="input" type="text" placeholder="Your Name" />
+													<textarea class="input" name="comentario"
+                                                              placeholder="Escribe aquí tu opinión"></textarea>
 												</div>
-												<div class="form-group">
-													<input class="input" type="email" placeholder="Email Address" />
-												</div>
-												<div class="form-group">
-													<textarea class="input" placeholder="Your review"></textarea>
-												</div>
+                                                <?php echo "<input style='display: none' 
+                                                value='$idU' name='idVendedor'>"; ?>
 												<div class="form-group">
 													<div class="input-rating">
-														<strong class="text-uppercase">Your Rating: </strong>
+														<strong
+                                                                class="text-uppercase">Valoración: </strong>
 														<div class="stars">
-															<input type="radio" id="star5" name="rating" value="5" /><label for="star5"></label>
-															<input type="radio" id="star4" name="rating" value="4" /><label for="star4"></label>
-															<input type="radio" id="star3" name="rating" value="3" /><label for="star3"></label>
-															<input type="radio" id="star2" name="rating" value="2" /><label for="star2"></label>
-															<input type="radio" id="star1" name="rating" value="1" /><label for="star1"></label>
+															<input type="radio" id="star5" name="puntaje" value="5"/><label for="star5"></label>
+															<input type="radio" id="star4" name="puntaje" value="4" /><label for="star4"></label>
+															<input type="radio" id="star3" name="puntaje" value="3" /><label for="star3"></label>
+															<input type="radio" id="star2" name="puntaje" value="2" /><label for="star2"></label>
+															<input type="radio" id="star1" name="puntaje" value="1" /><label for="star1"></label>
 														</div>
 													</div>
 												</div>
@@ -439,11 +426,6 @@ while($f=mysqli_fetch_array($resultado)){
 														
 										</div>
 									</div>
-
-<?php
-	}// Fin del while producto
-?>
-
 								</div>
 								
 									<div id="tab4" class="tab-pane fade in">
@@ -574,110 +556,11 @@ while($f=mysqli_fetch_array($resultado)){
 
 
 	<!-- FOOTER -->
-	<footer id="footer" class="section section-grey">
-		<!-- container -->
-		<div class="container">
-			<!-- row -->
-			<div class="row">
-				<!-- footer widget -->
-				<div class="col-md-3 col-sm-6 col-xs-6">
-					<div class="footer">
-						<!-- footer logo -->
-						<div class="footer-logo">
-							<a class="logo" href="#">
-		            <img src="./img/logo.png" alt="">
-		          </a>
-						</div>
-						<!-- /footer logo -->
-
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-
-						<!-- footer social -->
-						<ul class="footer-social">
-							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
-							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-							<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-						</ul>
-						<!-- /footer social -->
-					</div>
-				</div>
-				<!-- /footer widget -->
-
-				<!-- footer widget -->
-				<div class="col-md-3 col-sm-6 col-xs-6">
-					<div class="footer">
-						<h3 class="footer-header">My Account</h3>
-						<ul class="list-links">
-							<li><a href="#">My Account</a></li>
-							<li><a href="#">My Wishlist</a></li>
-							<li><a href="#">Compare</a></li>
-							<li><a href="#">Checkout</a></li>
-							<li><a href="#">Login</a></li>
-						</ul>
-					</div>
-				</div>
-				<!-- /footer widget -->
-
-				<div class="clearfix visible-sm visible-xs"></div>
-
-				<!-- footer widget -->
-				<div class="col-md-3 col-sm-6 col-xs-6">
-					<div class="footer">
-						<h3 class="footer-header">Customer Service</h3>
-						<ul class="list-links">
-							<li><a href="#">About Us</a></li>
-							<li><a href="#">Shiping & Return</a></li>
-							<li><a href="#">Shiping Guide</a></li>
-							<li><a href="#">FAQ</a></li>
-						</ul>
-					</div>
-				</div>
-				<!-- /footer widget -->
-
-				<!-- footer subscribe -->
-				<div class="col-md-3 col-sm-6 col-xs-6">
-					<div class="footer">
-						<h3 class="footer-header">Stay Connected</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
-						<form>
-							<div class="form-group">
-								<input class="input" placeholder="Enter Email Address">
-							</div>
-							<button class="primary-btn">Join Newslatter</button>
-						</form>
-					</div>
-				</div>
-				<!-- /footer subscribe -->
-			</div>
-			<!-- /row -->
-			<hr>
-			<!-- row -->
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2 text-center">
-					<!-- footer copyright -->
-					<div class="footer-copyright">
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-					</div>
-					<!-- /footer copyright -->
-				</div>
-			</div>
-			<!-- /row -->
-		</div>
-		<!-- /container -->
-	</footer>
-	<!-- /FOOTER -->
-
-	<!-- jQuery Plugins -->
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/slick.min.js"></script>
-	<script src="js/nouislider.min.js"></script>
-	<script src="js/jquery.zoom.min.js"></script>
-	<script src="js/main2.js"></script>
+    <?php
+    include_once("footer.php");
+    ?>
+    <script src="js/maps.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAiq3xISXSZYgkd9GDAOdajy4NK2d3L7dY&callback=iniciarMap2"></script>
 
 </body>
 
